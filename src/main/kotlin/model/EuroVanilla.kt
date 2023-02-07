68ef11f1aa1cc50a70ca1efaab446d0d3d9cb967
Luiz Cartolano <luiz.cartolano@kooltra.com>
2023-02-06 23:36:34 -0300

    fixes on eurovanilla calculator

--- src/main/kotlin/model/EuroVanilla.kt
+++ src/main/kotlin/model/EuroVanilla.kt
@@ -1,7 +1,6 @@
 package model
 
 data class EuroVanilla(
-    val underlying: Double,
     val strike: Double,
     val tenor: Double,
     val sigma: Double,
--- src/main/kotlin/model/EuroVanillaPrice.kt
+++ src/main/kotlin/model/EuroVanillaPrice.kt
@@ -1,6 +1,7 @@
 package model
 
 import util.NormalDistribution
+import util.roundTo
 import kotlin.math.E
 import kotlin.math.exp
 import kotlin.math.log
@@ -12,15 +13,15 @@ const val NATURAL_BASE = E
 class EuroVanillaPrice {
     private val normalDistribution = NormalDistribution()
 
-    private fun blackFirstDerivative(spot: Double, vanilla: EuroVanilla): Double {
+    internal fun blackFirstDerivative(spot: Double, vanilla: EuroVanilla): Double {
         val eqPart1 = log(spot / vanilla.strike, NATURAL_BASE)
-        val eqPart2 = (vanilla.foreignRate + vanilla.sigma.pow(2) / 2) * vanilla.tenor
+        val eqPart2 = ((vanilla.interestRate - vanilla.foreignRate) + vanilla.sigma.pow(2) / 2) * vanilla.tenor
         val eqPart3 = vanilla.sigma * sqrt(vanilla.tenor)
 
         return (eqPart1 + eqPart2) / eqPart3
     }
 
-    private fun blackSecondDerivative(spot: Double, vanilla: EuroVanilla): Double {
+    internal fun blackSecondDerivative(spot: Double, vanilla: EuroVanilla): Double {
         return this.blackFirstDerivative(spot, vanilla) - vanilla.sigma * sqrt(vanilla.tenor)
     }
 
@@ -28,33 +29,39 @@ class EuroVanillaPrice {
         val blackFirstDerivativeCalc = this.blackFirstDerivative(spot, vanilla)
         val blackSecondDerivativeCalc = this.blackSecondDerivative(spot, vanilla)
 
-        val eqPart1 = spot * exp((vanilla.foreignRate - vanilla.interestRate) * vanilla.tenor)
-        val eqPart2 = normalDistribution.cumulativeDistribution(blackFirstDerivativeCalc) -
-            vanilla.strike * exp(-vanilla.interestRate * vanilla.tenor)
-        val eqPart3 = normalDistribution.cumulativeDistribution(blackSecondDerivativeCalc)
+        // S0 * e^(-qT) * N(d1)
+        val eqPart1 = spot * exp(-vanilla.foreignRate * vanilla.tenor) *
+                normalDistribution.cumulativeDistribution(blackFirstDerivativeCalc)
+        // K * e^(-rT) * N(d2)
+        val eqPart2 = vanilla.strike * exp(-vanilla.interestRate * vanilla.tenor) *
+                normalDistribution.cumulativeDistribution(blackSecondDerivativeCalc)
 
-        return eqPart1 * eqPart2 * eqPart3
+        // C = S0 * e^(-qT) * N(d1) - K * e^(-rT) * N(d2)
+        return eqPart1 - eqPart2
     }
 
     private fun euroVanillaPut(spot: Double, vanilla: EuroVanilla): Double {
         val blackFirstDerivativeCalc = this.blackFirstDerivative(spot, vanilla)
         val blackSecondDerivativeCalc = this.blackSecondDerivative(spot, vanilla)
 
+        // K * e^(-rT) * N(-d2)
         val eqPart1 = vanilla.strike * exp(-vanilla.interestRate * vanilla.tenor) *
             normalDistribution.cumulativeDistribution(-blackSecondDerivativeCalc)
-        val eqPart2 = spot * exp((vanilla.foreignRate - vanilla.interestRate) * vanilla.tenor) *
+        // S0 * e^(-qT) * N(-d1)
+        val eqPart2 = spot * exp(-vanilla.foreignRate * vanilla.tenor) *
             normalDistribution.cumulativeDistribution(-blackFirstDerivativeCalc)
 
+        // P = K * e^(-rT) * N(-d2) - S0 * e^(-qT) * N(-d1)
         return eqPart1 - eqPart2
     }
 
-    fun calculate(spot: Double, vanilla: EuroVanilla, isCall: Boolean): Double {
+    fun calculate(spot: Double, vanilla: EuroVanilla, isCall: Boolean, decimalNumbers: Int = 2): Double {
         val price = if (isCall) {
             euroVanillaCall(spot, vanilla)
         } else {
             euroVanillaPut(spot, vanilla)
         }
 
-        return price
+        return price.roundTo(decimalNumbers)
     }
 }
